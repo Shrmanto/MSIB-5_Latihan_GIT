e3b4ca7e87166f210cdcf9ab45b6da79325cd755
Shrmanto <shrmantoch@gmail.com>
2023-08-23 18:34:20 +0700

    Upload

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>MSIB 5 Latihan GIT</title>
     <link rel="stylesheet" href="style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
     <link href="/fontawesome-free-6.4.2-web/css/fontawesome.css" rel="stylesheet">
@@ -24,6 +24,12 @@
             $_SESSION[$sessionKey]++;
             header("Location: ".$_SERVER['PHP_SELF']);
         }
+
+        $nama = "Suhermanto";
+        $univ = "Universitas Jember";
+        $jurusan = "Informatika";
+        $bidang = "Full Stack Web Development";
+        $mitra = "PT Gits Indonesia";
     ?>
     <div class="content">
         <div class="card d-flex shadow-lg rounded-5">
@@ -38,10 +44,10 @@
                         <img src="/img/profil.jpg" class="orang-1 rounded-circle" width="30" height="30" alt="">
                         <img src="/img/profil.jpg" class="orang-2 rounded-circle" width="30" height="30" alt="">
                         <img src="/img/profil.jpg" class="orang-3 rounded-circle" width="30" height="30" alt="">
-                        <p style="margin-left: 35px; margin-top: 10px;">Disukai oleh <?php echo $_SESSION['angka']; ?> orang</p>
+                        <p style="margin-left: 35px; margin-top: 10px;">Disukai oleh <span style="font-weight: 700;"><?php echo $_SESSION['angka']; ?></span> orang</p>
                     </div>
                 </div>
-                <p class="card-text mt-2">Halo, Nama saya <span style="font-weight: 700;">Suhermanto</span>, Saya merupakan mahasiswa <span style="font-weight: 700;">informatika</span> dari <span style="font-weight: 700;">Universitas Jember</span>. Saya telah dipilih sebagai <span style="font-weight: 700;">Full Stack Web Development</span> di <span style="font-weight: 700;">PT Gits Indonesia</span>. Hobi saya yaitu bermain <span style="font-weight: 700;">Game</span>, <span style="font-weight: 700;">Makan</span> (Prioritas) dan <span style="font-weight: 700;">Olahraga</span> (kalo inget). Rencana kedepannya mau menjadi Rektor kampus😂.
+                <p class="card-text mt-2">Halo, Nama saya <span style="font-weight: 700;"><?php echo $nama ?></span>, Saya merupakan mahasiswa <span style="font-weight: 700;"><?php echo $jurusan ?></span> dari <span style="font-weight: 700;"><?php echo $univ ?></span>. Saya telah dipilih sebagai <span style="font-weight: 700;"><?php echo $bidang ?></span> di <span style="font-weight: 700;"><?php echo $mitra ?></span>. Hobi saya yaitu bermain <span style="font-weight: 700;">Game</span>, <span style="font-weight: 700;">Makan</span> (Prioritas) dan <span style="font-weight: 700;">Olahraga</span> (kalo inget). Rencana kedepannya mau menjadi Rektor kampus😂.
 <br><span style="font-weight: 700;">#YOURPARTNERTOSCALEIMPACTWITHTECHNOLOGY</span></p>
                 <div class="content-link mt-5 mb-3">
                     <a href="" class="instagram-icon mx-2"><i class="fa-brands fa-instagram fa-2xl" style="color: #2D2D39;"></i></a>
